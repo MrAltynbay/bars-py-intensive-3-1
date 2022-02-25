ead01d1362e5ff580b687c37255e694ce0542698
Salavat <altinbaevsal@gmail.com>
2022-02-25 20:15:04 +0300

    [ADD] all task solved

--- tasks/magic_method/task_1/implementation.py
+++ tasks/magic_method/task_1/implementation.py
@@ -1,24 +1,43 @@
 class Multiplier:
-    def __init__(self):
-        pass
+    """Родительский класс для арифмитических операций"""
+    def __init__(self, value):
+        self.value = value
+
+    def get_value(self):
+        return int(self.value)
+
+    def __add__(self, other):
+        new_val = self.value + other.value
+        return Multiplier(new_val)
+
+    def __sub__(self, other):
+        new_val = self.value - other.value
+        return Multiplier(new_val)
+
+    def __mul__(self, other):
+        new_val = self.value * other.value
+        return Multiplier(new_val)
+
+    def __truediv__(self, other):
+        new_val = self.value / other.value
+        return Multiplier(new_val)
 
 
 class Hundred(Multiplier):
     """Множитель на 100"""
-    def __init__(self):
-        pass
+    def __init__(self, value):
+        super().__init__(value * 100)
 
 
 class Thousand(Multiplier):
     """Множитель на 1 000"""
-    def __init__(self):
-        pass
+    def __init__(self, value):
+        super().__init__(value * 1000)
 
 
 class Million(Multiplier):
     """Множитель на 1 000 000"""
-    def __init__(self):
-        pass
-
+    def __init__(self, value):
+        super().__init__(value * 1000000)
 
 
--- tasks/magic_method/task_2/implementation.py
+++ tasks/magic_method/task_2/implementation.py
@@ -1,8 +1,24 @@
 class MathClock:
     def __init__(self):
-        pass
+        self.minutes = 0
 
     def get_time(self):
-        return 0
+        minutes = self.minutes % 60
+        hours = (self.minutes // 60) % 24
+        return f'{hours:02}:{minutes:02}'
 
+    def __add__(self, other):
+        self.minutes += other
+        return self.minutes
 
+    def __sub__(self, other):
+        self.minutes -= other
+        return self.minutes
+
+    def __mul__(self, other):
+        self.minutes = self.minutes + other * 60
+        return self.minutes
+
+    def __truediv__(self, other):
+        self.minutes = self.minutes - other * 60
+        return self.minutes
